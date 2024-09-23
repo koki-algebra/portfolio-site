9630dbf863d06cc69e59f4fc2e3c120a4aa8d38c
koki-algebra <biz.kokiyamada1022@gmail.com>
2024-09-23 21:25:41 +0900

    fix auth interceptor

--- backend/internal/presentation/interseptor/auth.go
+++ backend/internal/presentation/interseptor/auth.go
@@ -26,35 +26,30 @@ func NewAuthInterceptor(repo repository.User) *AuthInterceptor {
 func (a *AuthInterceptor) Auth() connect.UnaryInterceptorFunc {
 	return func(next connect.UnaryFunc) connect.UnaryFunc {
 		return func(ctx context.Context, req connect.AnyRequest) (connect.AnyResponse, error) {
-			resp, err := next(ctx, req)
-			if err != nil {
-				return resp, err
-			}
-
 			rawTokenString := req.Header().Get("Authorization")
 			if rawTokenString == "" {
-				return resp, connect.NewError(connect.CodeUnauthenticated, fmt.Errorf("id token is required"))
+				return nil, connect.NewError(connect.CodeUnauthenticated, fmt.Errorf("id token is required"))
 			}
 
 			audience := config.Env.GoogleClientID
 
 			payload, err := idtoken.Validate(ctx, rawTokenString, audience)
 			if err != nil {
-				return resp, connect.NewError(connect.CodeUnauthenticated, fmt.Errorf("invalid id token: %v", err))
+				return nil, connect.NewError(connect.CodeUnauthenticated, fmt.Errorf("invalid id token: %v", err))
 			}
 
 			email, ok := a.getEmail(payload.Claims)
 			if !ok {
-				return resp, connect.NewError(connect.CodeInvalidArgument, fmt.Errorf("the id token does not contain an email"))
+				return nil, connect.NewError(connect.CodeInvalidArgument, fmt.Errorf("the id token does not contain an email"))
 			}
 
 			user, err := a.repo.FindByEmail(ctx, email)
 			if err != nil {
 				if errors.Is(err, repository.ErrNotFound) {
-					return resp, connect.NewError(connect.CodeNotFound, fmt.Errorf("user not found"))
+					return nil, connect.NewError(connect.CodeNotFound, fmt.Errorf("user not found"))
 				}
 
-				return resp, connect.NewError(connect.CodeInternal, fmt.Errorf("failed to find user: %v", err))
+				return nil, connect.NewError(connect.CodeInternal, fmt.Errorf("failed to find user: %v", err))
 			}
 
 			ctx = model.ContextWithUser(ctx, user)
