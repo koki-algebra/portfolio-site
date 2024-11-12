e4c67765d80723cc781d206ea3e4471457359d6b
Koki Yamada <54975589+koki-algebra@users.noreply.github.com>
2024-11-12 23:51:48 +0900

    fix schema (#21)

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# portfolio-site
+# Portfolio Site
+
 Portfolio Site
--- database/sql/1_create_tables.sql
+++ database/sql/1_create_tables.sql
@@ -2,9 +2,11 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp"; -- noqa: RF05
 
 CREATE TABLE IF NOT EXISTS users (
     user_id UUID NOT NULL DEFAULT uuid_generate_v4(),
+    auth_id TEXT NOT NULL,
     email TEXT NOT NULL,
     PRIMARY KEY (user_id),
-    CONSTRAINT uq_users_01 UNIQUE (email)
+    CONSTRAINT uq_users_01 UNIQUE (auth_id),
+    CONSTRAINT uq_users_02 UNIQUE (email)
 );
 
-CREATE INDEX idx_users_01 ON users (email);
+CREATE INDEX idx_users_01 ON users (auth_id);
