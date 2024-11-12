0bfca4e8a541a2e2c577bd37f5eba2c452ac51fe
koki-algebra <biz.kokiyamada1022@gmail.com>
2024-11-12 22:00:24 +0900

    move models

---
+++ backend/internal/domain/model/caller/context.go
@@ -0,0 +1,27 @@
+package callermodel
+
+import (
+	"context"
+
+	usermodel "backend/internal/domain/model/user"
+)
+
+type ctxCallerKey struct{}
+
+func ContextWithCaller(ctx context.Context, user *usermodel.User) context.Context {
+	if user == nil {
+		return ctx
+	}
+
+	caller := &Caller{
+		userID: user.UserID,
+		email:  user.Email,
+	}
+
+	return context.WithValue(ctx, ctxCallerKey{}, caller)
+}
+
+func CallerFromContext(ctx context.Context) (*Caller, bool) {
+	caller, ok := ctx.Value(ctxCallerKey{}).(*Caller)
+	return caller, ok
+}
